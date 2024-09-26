09a0871a4874f5dddbf176e4dcaf11bbb744c280
oanh2000 <oanh0109jk@gmail.com>
2024-09-26 19:35:57 +0700

    create_tool_memefi

---
+++ bot/config/proxies.js
@@ -0,0 +1,78 @@
+//Currently only http and https proxies are supported.
+//HTTP proxies are not supported as they required a completely different connection type.
+
+const proxies = 
+[
+  {
+    "ip": "192.53.66.68",
+    "port": 6174,
+    "protocol": "http",
+    "username": "sndjyfwk",
+    "password": "8w2yu1ldrffd"
+  },
+  {
+    "ip": "104.243.210.104",
+    "port": 5752,
+    "protocol": "http",
+    "username": "sndjyfwk",
+    "password": "8w2yu1ldrffd"
+  },
+  {
+    "ip": "192.46.188.26",
+    "port": 5685,
+    "protocol": "http",
+    "username": "sndjyfwk",
+    "password": "8w2yu1ldrffd"
+  },
+  {
+    "ip": "45.196.54.148",
+    "port": 6727,
+    "protocol": "http",
+    "username": "sndjyfwk",
+    "password": "8w2yu1ldrffd"
+  },
+  {
+    "ip": "216.170.122.31",
+    "port": 6069,
+    "protocol": "http",
+    "username": "sndjyfwk",
+    "password": "8w2yu1ldrffd"
+  },
+  {
+    "ip": "62.164.242.97",
+    "port": 8674,
+    "protocol": "http",
+    "username": "sndjyfwk",
+    "password": "8w2yu1ldrffd"
+  },
+  {
+    "ip": "45.196.40.29",
+    "port": 6107,
+    "protocol": "http",
+    "username": "sndjyfwk",
+    "password": "8w2yu1ldrffd"
+  },
+  {
+    "ip": "154.194.24.211",
+    "port": 5821,
+    "protocol": "http",
+    "username": "sndjyfwk",
+    "password": "8w2yu1ldrffd"
+  },
+  {
+    "ip": "45.196.52.249",
+    "port": 6264,
+    "protocol": "http",
+    "username": "sndjyfwk",
+    "password": "8w2yu1ldrffd"
+  },
+  {
+    "ip": "63.246.130.141",
+    "port": 6342,
+    "protocol": "http",
+    "username": "sndjyfwk",
+    "password": "8w2yu1ldrffd"
+  }
+];
+
+module.exports = proxies;
